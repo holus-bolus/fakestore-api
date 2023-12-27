dd6a00b658bb87334fd1cde4c093ec1959a65f58
holus-bolus <ustinovoleksij@gmail.com>
2023-12-27 11:59:35 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Products from "./components/Products/Products.tsx";
+
+function App() {
+
+
+    return (
+        <>
+            <Products/>
+        </>
+    )
+}
+
+export default App
